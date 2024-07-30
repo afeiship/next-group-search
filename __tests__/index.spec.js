a7b0b07067588c3911e00b00e2e3636a13ca4357
afeiship <1290657123@qq.com>
2024-07-30 23:04:48 +0800

    feat: add obj/array/list case

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,7 +1,7 @@
 require('../src');
 
-describe('api.basic test', () => {
-  test('groupSearch should return a list of groups', function () {
+describe('nx.groupSearch function', () => {
+  test('01/group is object', function () {
     const g1 = {
       restock: [
         { name: 'asparagus', type: 'vegetables', quantity: 5 },
@@ -35,5 +35,51 @@ describe('api.basic test', () => {
       ],
       ok: []
     });
+  });
+
+  test('02/group is an array', () => {
+    const g1 = [
+      {
+        groupKey: 'restock',
+        items: [
+          { name: 'asparagus', type: 'vegetables', quantity: 5 },
+          { name: 'bananas', type: 'fruit', quantity: 0 },
+          { name: 'cherries', type: 'fruit', quantity: 5 }
+        ]
+      },
+      {
+        groupKey: 'ok',
+        items: [
+          { name: 'goat', type: 'meat', quantity: 23 },
+          { name: 'fish', type: 'meat', quantity: 22 }
+        ]
+      }
+    ];
+    const opts = { itemsKey: 'items', groupKey: 'groupKey' };
+    const res01 = nx.groupSearch(g1, 'ALL', opts);
+    const res02 = nx.groupSearch(g1, '', opts);
+    const res03 = nx.groupSearch(g1, '*', opts);
+    const res04 = nx.groupSearch(g1, '@', { allKeys: ['@'], ...opts });
+    const res1 = nx.groupSearch(g1, 'meat', {
+      ...opts,
+      callback: ({ item, keyword }) => {
+        return item.type === keyword;
+      }
+    });
+
+    expect(res01).toEqual(g1);
+    expect(res02).toEqual(g1);
+    expect(res03).toEqual(g1);
+    expect(res04).toEqual(g1);
+    expect(res1).toEqual([
+      { groupKey: 'restock', items: [] },
+      {
+        groupKey: 'ok',
+        items: [
+          { name: 'goat', type: 'meat', quantity: 23 },
+          { name: 'fish', type: 'meat', quantity: 22 }
+        ]
+      }
+    ]);
   });
 });
--- src/index.js
+++ src/index.js
@@ -2,21 +2,39 @@ import nx from '@jswork/next';
 
 const defaults = {
   allKeys: ['ALL', '', '*'],
+  groupKey: 'groupKey',
+  itemsKey: 'items'
 };
 
 nx.groupSearch = function (inGroup, inKeyword, inOptions) {
   const options = nx.mix(null, defaults, inOptions);
   const keyword = inKeyword.trim();
-  const result = {};
-  nx.forIn(inGroup, (groupName, items) => {
-    if (Array.isArray(items)) {
-      result[groupName] = items.filter((item, index) => {
-        if (options.allKeys.includes(keyword)) return true;
-        return options.callback({ item, index, keyword, groupName });
-      });
-    }
-  });
-  return result;
+  if (Array.isArray(inGroup)) {
+    return inGroup.map((group) => {
+      const items = group[options.itemsKey];
+      if (Array.isArray(items)) {
+        return {
+          ...group,
+          [options.itemsKey]: items.filter((item, index) => {
+            if (options.allKeys.includes(keyword)) return true;
+            return options.callback({ item, index, keyword, groupKey: group.groupKey });
+          })
+        };
+      }
+      return group;
+    });
+  } else {
+    const result = {};
+    nx.forIn(inGroup, (groupKey, items) => {
+      if (Array.isArray(items)) {
+        result[groupKey] = items.filter((item, index) => {
+          if (options.allKeys.includes(keyword)) return true;
+          return options.callback({ item, index, keyword, groupKey });
+        });
+      }
+    });
+    return result;
+  }
 };
 
 if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
