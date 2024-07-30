a56e9d2c56e05c98e450bccdfbf80b9f44026624
afeiship <1290657123@qq.com>
2024-07-30 23:09:19 +0800

    refactor: create-fitler-fn

--- src/index.js
+++ src/index.js
@@ -9,16 +9,18 @@ const defaults = {
 nx.groupSearch = function (inGroup, inKeyword, inOptions) {
   const options = nx.mix(null, defaults, inOptions);
   const keyword = inKeyword.trim();
+  const createFilterFn = (groupKey) => (item, index) => {
+    if (options.allKeys.includes(keyword)) return true;
+    return options.callback({ item, index, keyword, groupKey });
+  };
+
   if (Array.isArray(inGroup)) {
     return inGroup.map((group) => {
       const items = group[options.itemsKey];
       if (Array.isArray(items)) {
         return {
           ...group,
-          [options.itemsKey]: items.filter((item, index) => {
-            if (options.allKeys.includes(keyword)) return true;
-            return options.callback({ item, index, keyword, groupKey: group.groupKey });
-          })
+          [options.itemsKey]: items.filter(createFilterFn(group[options.groupKey]))
         };
       }
       return group;
@@ -27,10 +29,7 @@ nx.groupSearch = function (inGroup, inKeyword, inOptions) {
     const result = {};
     nx.forIn(inGroup, (groupKey, items) => {
       if (Array.isArray(items)) {
-        result[groupKey] = items.filter((item, index) => {
-          if (options.allKeys.includes(keyword)) return true;
-          return options.callback({ item, index, keyword, groupKey });
-        });
+        result[groupKey] = items.filter(createFilterFn(groupKey));
       }
     });
     return result;
